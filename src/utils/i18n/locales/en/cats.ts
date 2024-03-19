beb6d8fecdfcb521c1102e27466f2ee834f2b56d
Daniel Quero <danielquero95@gmail.com>
2024-03-20 00:25:35 +0100

    Added new translations

---
+++ src/utils/i18n/locales/en/cats.ts
@@ -0,0 +1,7 @@
+export default {
+	cat: 'Cat',
+	cats: 'Cats',
+	catDescription: 'Citicens for your villlage.',
+	attractCat: 'Attract cat',
+	attractCatDescription: `Make a trap to attract a new Cat for your village, once here will be glad to help you. Each time will need more resources.`,
+}
--- src/utils/i18n/locales/en/resources.ts
+++ src/utils/i18n/locales/en/resources.ts
@@ -1,6 +1,11 @@
 export default {
 	wood: 'Wood',
-	food: 'Food',
+	woodDescription: 'Wood from the forest nearby, great for buildings.',
 	pickupWood: 'Pickup wood',
+	pickupWoodDescription: 'Chop some wood for your camp, might be useful.',
+	food: 'Food',
 	pickupFood: 'Pickup food',
+	foodDescription: 'Delicious fish from the closest lake.',
+	pickupFoodDescription: 'Go catch a fish, everyone need to eat!',
+	freeHouse: 'House free',
 }
