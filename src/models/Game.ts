bf7d05d6d8411ea72c740010146d85814186b607
Daniel Quero <danielquero95@gmail.com>
2024-03-20 00:25:59 +0100

    Delete useless code (for now)

--- src/models/Game.ts
+++ src/models/Game.ts
@@ -10,7 +10,6 @@ export class Game {
 	constructor(initialFood: number, maxBaseWood: number, initialWood: number, maxBaseFood: number) {
 		this.resourcesManager = new ResourcesManager(initialWood, maxBaseWood, initialFood, maxBaseFood)
 		this.buildingsManager = new BuildingsManager(this.resourcesManager)
-		this.resourcesManager.setBuildingsManager(this.buildingsManager)
 		this.catsManager = new CatsManager(this.resourcesManager, this.buildingsManager)
 	}
 }
